1cfd3efa41c4369475e067c9907938a5d65c7b2b
John Syomochkin <info@syomochkin.xyz>
2019-01-14 00:19:33 +0300

    выделение действий с графиком валют в отдельный объект

--- src/widget/exchangeRate.js
+++ src/widget/exchangeRate.js
@@ -31,6 +31,123 @@ async function getCurrencyRate({id, dateFrom, dateTo}) {
         }))
 }
 
+const exchangeRateCanvas = {
+    constructor() {
+        this.initScales()
+            .initAxis()
+            .initLine()
+            .initAnimation()
+    },
+
+    async update(state) {
+        if (!state.id && !state.dateFrom && !state.dateTo) {
+            return
+        }
+        this.dataset = await getCurrencyRate(state)
+        this.updateScales(state)
+            .updateAxis()
+            .updateLine()
+    },
+
+    initScales() {
+        this.scaleRate = d3.scaleLinear()
+            .range([this.bottomY, this.topY]);
+        this.scaleDate = d3.scaleTime()
+            .range([this.leftX, this.rightX]);
+        return this
+    },
+
+    initAxis() {
+        this.axisRateFn = d3.axisLeft()
+        this.axisDateFn = d3.axisBottom()
+            .tickFormat(dateFormat)
+        this.axisRate = this.svg.append('g')
+            .attr('transform', `translate(${this.leftX}, 0)`)
+        this.axisDate = this.svg.append('g')
+            .attr('transform', `translate(0, ${this.bottomY})`)
+        return this
+    },
+
+    initLine() {
+        this.lineFn = d3.line()
+        this.line = this.svg.append('path')
+            .attr('fill', 'none')
+            .attr('stroke', lineColor)
+            .attr('stroke-width', 1)
+        this.markers = this.svg.append('g')
+            .classed('markers', true)
+        return this
+    },
+
+
+    updateScales({dateFrom, dateTo}) {
+        let [minRate, maxRate] = d3.extent(this.dataset, d => d.rate)
+        minRate *= .9
+        maxRate *= 1.1
+        const dateDomain = [
+            d3.timeDay.offset(toDate(dateFrom), -1),
+            d3.timeDay.offset(toDate(dateTo), 1)
+        ]
+        this.scaleRate.domain([minRate, maxRate])
+        this.scaleDate.domain(dateDomain)
+        return this
+    },
+
+    updateAxis() {
+        this.axisRateFn.scale(this.scaleRate)
+        const dateTicksCount = Math.min(d3.timeDay.count(...this.scaleDate.domain()), 10);
+        this.axisDateFn.scale(this.scaleDate)
+            .ticks(dateTicksCount)
+        this.axisRate.transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .call(this.axisRateFn)
+        this.axisDate.transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .call(this.axisDateFn)
+        return this
+    },
+
+    updateLine() {
+        this.lineFn
+            .x(d => this.scaleDate(d.date))
+            .y(d => this.scaleRate(d.rate))
+        this.line.datum(this.dataset)
+            .transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .attr('d', d => this.lineFn(d))
+        const markers = this.markers.selectAll('.marker')
+            .data(this.dataset, d => d.date)
+        markers.exit()
+            .transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .attr('cy', d => this.scaleRate(d.rate))
+            .attr('cx', d => this.scaleDate(d.date))
+            .attr('r', 0)
+            .remove()
+        markers.enter()
+            .append('circle')
+            .classed('marker', true)
+            .attr('cy', d => this.scaleRate(d.rate))
+            .attr('cx', d => this.scaleDate(d.date))
+            .attr('fill', markerColor)
+            .attr('r', 0)
+            .transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .attr('r', 4)
+        markers.transition()
+            .duration(this.duration)
+            .ease(this.ease)
+            .attr('cy', d => this.scaleRate(d.rate))
+            .attr('cx', d => this.scaleDate(d.date))
+        return this
+    }
+}
+
 const excangeRateWidget = async function(options, context) {
     const widget = widgetFactory(options, context);
     const {controls} = widget;
@@ -60,121 +177,9 @@ const excangeRateWidget = async function(options, context) {
         .addControl(dateToControl)
         .addControl(currencyControl, 6)
     
-    const canvas = widget.appendChart();
-    const unsubscribe = store.subscribe(() => updateCanvas(canvas, store.getState()))
-    
-    canvas.initScales = function() {
-        this.scaleRate = d3.scaleLinear()
-            .range([this.bottomY, this.topY]);
-        this.scaleDate = d3.scaleTime()
-            .range([this.leftX, this.rightX]);
-    }
-
-    canvas.initAxis = function() {
-        this.axisRateFn = d3.axisLeft()
-        this.axisDateFn = d3.axisBottom()
-            .tickFormat(dateFormat)
-        this.axisRate = this.svg.append('g')
-            .attr('transform', `translate(${this.leftX}, 0)`)
-        this.axisDate = this.svg.append('g')
-            .attr('transform', `translate(0, ${this.bottomY})`)
-    }
-
-    canvas.initLine = function() {
-        this.lineFn = d3.line()
-        this.line = this.svg.append('path')
-            .attr('fill', 'none')
-            .attr('stroke', lineColor)
-            .attr('stroke-width', 1)
-        this.markers = this.svg.append('g')
-            .classed('markers', true)
-    }
-
-    canvas.initAnimation = function() {
-        this.duration = 500;
-        this.ease = d3.easeQuadInOut;
-    }
-
-    canvas.updateScales = function(dataset, {dateFrom, dateTo}) {
-        let [minRate, maxRate] = d3.extent(dataset, d => d.rate)
-        minRate *= .9
-        maxRate *= 1.1
-        const dateDomain = [
-            d3.timeDay.offset(toDate(dateFrom), -1),
-            d3.timeDay.offset(toDate(dateTo), 1)
-        ]
-        this.scaleRate.domain([minRate, maxRate])
-        this.scaleDate.domain(dateDomain)
-    }
-
-    canvas.updateAxis = function() {
-        this.axisRateFn.scale(this.scaleRate)
-        const dateTicksCount = Math.min(d3.timeDay.count(...this.scaleDate.domain()), 10);
-        this.axisDateFn.scale(this.scaleDate)
-            .ticks(dateTicksCount)
-        this.axisRate.transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .call(this.axisRateFn)
-        this.axisDate.transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .call(this.axisDateFn)
-    }
-
-    canvas.updateLine = function(dataset) {
-        this.lineFn
-            .x(d => this.scaleDate(d.date))
-            .y(d => this.scaleRate(d.rate))
-        this.line.datum(dataset)
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .attr('d', d => this.lineFn(d))
-        const markers = this.markers.selectAll('.marker')
-            .data(dataset, d => d.date)
-        markers.exit()
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .attr('cy', d => this.scaleRate(d.rate))
-            .attr('cx', d => this.scaleDate(d.date))
-            .attr('r', 0)
-            .remove()
-        markers.enter()
-            .append('circle')
-            .classed('marker', true)
-            .attr('cy', d => this.scaleRate(d.rate))
-            .attr('cx', d => this.scaleDate(d.date))
-            .attr('fill', markerColor)
-            .attr('r', 0)
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .attr('r', 4)
-        markers.transition()
-            .duration(this.duration)
-            .ease(this.ease)
-            .attr('cy', d => this.scaleRate(d.rate))
-            .attr('cx', d => this.scaleDate(d.date))
-    }
-
-    canvas.initScales()
-    canvas.initAxis()
-    canvas.initLine()
-    canvas.initAnimation()
-
-    updateCanvas(canvas, initialState)
-};
-
-async function updateCanvas(canvas, state) {
-    if (!state.id && !state.dateFrom && !state.dateTo) {
-        return
-    }
-    const dataset = await getCurrencyRate(state)
-    canvas.updateScales(dataset, state)
-    canvas.updateAxis()
-    canvas.updateLine(dataset)
+    const canvas = widget.appendChart(exchangeRateCanvas)
+    const unsubscribe = store.subscribe(() => canvas.update(store.getState()))
+    canvas.update(initialState)
 }
 
 export default excangeRateWidget;
