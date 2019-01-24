05485721fe5e184768f05b181a6e8059c3eedb63
John Syomochkin <info@syomochkin.xyz>
2019-01-24 10:50:40 +0300

    добавлены линии прицела на hovered маркер, добавлены стили элемнтов графика, исправлена анимация под именованную

--- src/widget/exchangeRate/chart.js
+++ src/widget/exchangeRate/chart.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3'
 import axios from 'axios'
+import './chart.scss'
 
 
 const dateFormat = d3.timeFormat("%d.%m.%Y")
@@ -39,8 +40,8 @@ const exchangeRateChart = {
     constructor() {
         this.initScales()
             .initAxis()
+            .initAim()
             .initLines()
-            .initAnimation()
     },
 
     async update(state) {
@@ -83,6 +84,17 @@ const exchangeRateChart = {
         return this
     },
 
+    initAim() {
+        this.aimGroup = this.svg.append('g')
+            .classed('currency-aim', true)
+            .classed('currency-aim_hidden', true)
+        this.aimLineY = this.aimGroup.append('line')
+            .classed('currency-aim__line', true)
+        this.aimLineX = this.aimLineY.clone()
+            .classed('currency-aim__line_type_x', true)
+        this.aimLineY.classed('currency-aim__line_type_y', true)
+        return this
+    },
 
     updateScales({dateFrom, dateTo}) {
         if (!this.dataset.length) {
@@ -109,13 +121,9 @@ const exchangeRateChart = {
         const dateTicksCount = Math.min(d3.timeDay.count(...this.scaleDate.domain()), 10)
         this.axisDateFn.scale(this.scaleDate)
             .ticks(dateTicksCount)
-        this.axisRate.transition()
-            .duration(this.duration)
-            .ease(this.ease)
+        this.axisRate.transition(this.animSlow)
             .call(this.axisRateFn)
-        this.axisDate.transition()
-            .duration(this.duration)
-            .ease(this.ease)
+        this.axisDate.transition(this.animSlow)
             .call(this.axisDateFn)
         return this
     },
@@ -130,9 +138,7 @@ const exchangeRateChart = {
         const newLines = this.enterLines(lines)
         lines.merge(newLines)
             .datum(d => d.set)
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
+            .transition(this.animSlow)
             .attr('d', d => this.lineFn(d))
         this.updateMarkerGroups()
         return this
@@ -148,7 +154,8 @@ const exchangeRateChart = {
     },
 
     updateMarkerGroups() {
-        const markerGroups = this.markersContainer.selectAll('.currency-markers__group')
+        const markerGroups = this.markersContainer
+            .selectAll('.currency-markers__group')
             .data(this.dataset)
         markerGroups.exit().remove()
         const newMarkerGroups = markerGroups.enter()
@@ -165,9 +172,7 @@ const exchangeRateChart = {
             })), d => d.date)
         this.removeMarkers(markers)
         this.enterMarkers(markers)
-        markers.transition()
-            .duration(this.duration)
-            .ease(this.ease)
+        markers.transition(this.animSlow)
             .attr('cy', d => this.scaleRate(d.rate))
             .attr('cx', d => this.scaleDate(d.date))
         return this
@@ -175,9 +180,7 @@ const exchangeRateChart = {
 
     removeMarkers(markers) {
         markers.exit()
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
+            .transition(this.animSlow)
             .attr('cy', d => this.scaleRate(d.rate))
             .attr('cx', d => this.scaleDate(d.date))
             .attr('r', 0)
@@ -185,17 +188,60 @@ const exchangeRateChart = {
     },
 
     enterMarkers(markers) {
+        const self = this
         return markers.enter()
             .append('circle')
             .classed('currency-marker', true)
             .attr('cy', d => this.scaleRate(d.rate))
             .attr('cx', d => this.scaleDate(d.date))
-            .attr('fill', d => d3.color(this.color(d.i)).darker(1))
-            .attr('r', 0)
-            .transition()
-            .duration(this.duration)
-            .ease(this.ease)
+            .attr('fill', d => d3.color(this.color(d.i)))
             .attr('r', 4)
+            .attr('stroke', 'transparent')
+            .attr('stroke-width', 4)
+            .on('mouseover', function(d) {
+                d3.select(this)
+                    .interrupt(self.animFast)
+                    .transition(self.animFast)
+                    .attr('r', 8)
+                    .attr('stroke-width', 0)
+            })
+            .on('mouseout', function(d) {
+                d3.select(this)
+                    .interrupt(self.animFast)
+                    .transition(self.animFast)
+                    .attr('r', 4)
+                    .attr('stroke-width', 4)
+        })
+            .on('mouseover.aim', d => this.replaceAimLines(d))
+            .on('mouseout.aim', () => this.hideAimLines())
+    },
+
+    replaceAimLines({rate, date}) {
+        this.showAimLines()
+        this.aimLineY
+            .interrupt(self.animFast)
+            .transition(this.animFast)
+            .attr('x1', this.scaleDate(date))
+            .attr('x2', this.scaleDate.range()[0])
+            .attr('y1', this.scaleRate(rate))
+            .attr('y2', this.scaleRate(rate))
+        this.aimLineX
+            .interrupt(self.animFast)
+            .transition(this.animFast)
+            .attr('x1', this.scaleDate(date))
+            .attr('x2', this.scaleDate(date))
+            .attr('y1', this.scaleRate(rate))
+            .attr('y2', this.scaleRate.range()[0])
+    },
+
+    showAimLines() {
+        this.aimGroup
+            .classed('currency-aim_hidden', false)
+    },
+
+    hideAimLines() {
+        this.aimGroup
+            .classed('currency-aim_hidden', true)
     }
 }
 
