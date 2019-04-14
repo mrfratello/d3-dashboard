471ef1db07ef51efb5607ab650a321cc9a94ab11
John Syomochkin <info@syomochkin.xyz>
2019-04-14 17:58:11 +0300

    #3 подключение grid-layout, перемещение и resize "тени" виджета

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,19 @@
-import { ScrollPanel } from 'primereact/scrollpanel'
+import GridLayout from 'react-grid-layout'
 import { Widget } from 'Components/widget/Widget'
 import './Dashboard.scss'
 
 
-export const Dashboard = ({widgets=[]}) =>
-    <ScrollPanel style={{width: window.innerWidth, height: window.innerHeight}} className='Dashboard'>
-        <div className='Dashboard-Content'>
+export const Dashboard = ({widgets=[]}) => {
+    return (
+        <GridLayout className="layout" cols={6} rowHeight={30} width={window.innerWidth}>
             { widgets.map((widgetParams, i) =>
-                <Widget key={i} {...widgetParams} />) }
-        </div>
-    </ScrollPanel>
+                <div key={i} data-grid={{x: 2, y: 2, w: 3, h: 12}}>
+                    <Widget key={i} {...widgetParams} />
+                </div>
+                )
+            }
+        </GridLayout>
+    )
+}
 
 export default Dashboard
