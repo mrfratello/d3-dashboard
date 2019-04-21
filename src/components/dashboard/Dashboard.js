754210bea9525a84c038da2aa98253bbe6e12aa7
John Syomochkin <info@syomochkin.xyz>
2019-04-21 17:53:32 +0300

    #3 вынос стилей ExchangeRate в scss, изменение начальных размеров виджета

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ export const Dashboard = ({widgets=[]}) => {
     return (
         <GridLayout className="layout" cols={6} rowHeight={30} width={window.innerWidth}>
             { widgets.map((widgetParams, i) =>
-                <div key={i} data-grid={{x: 2, y: 2, w: 3, h: 12}}>
+                <div key={i} data-grid={{x: 0, y: 0, w: 4, h: 12, minH: 10, minW: 3}}>
                     <Widget key={i} {...widgetParams} />
                 </div>
                 )
--- src/components/exchangeRate/ExchangeRate.js
+++ src/components/exchangeRate/ExchangeRate.js
@@ -1,15 +1,16 @@
 import Measure from 'react-measure'
 import Control from 'Containers/exchangeRate/control/Control'
 import Chart from 'Containers/exchangeRate/chart/Chart'
+import './ExchangeRate.scss'
 
 export const ExchangeRate = ({model, fetchCurrencyRateList}) => {
     fetchCurrencyRateList(model)
     return (
-        <div style={{display:'flex', flexDirection:'column', height: '100%'}}>
+        <div className='ExchangeRate'>
             <Control model={model} />
             <Measure client>
                 {({measureRef, contentRect}) => (
-                    <div ref={measureRef} style={{flex:1, height: 0}}>
+                    <div ref={measureRef} className='ExchangeRate-Chart'>
                         <Chart widgetId={model.id}
                             width={contentRect.client.width || 10}
                             height={contentRect.client.height - 10 || 20} />
