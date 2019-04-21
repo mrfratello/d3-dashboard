ba8a496046126498329a4bbf6f26a1a26a8ad426
John Syomochkin <info@syomochkin.xyz>
2019-04-21 17:45:05 +0300

    #3 возврат ExchangeRate к stateless компоненту

--- src/components/exchangeRate/ExchangeRate.js
+++ src/components/exchangeRate/ExchangeRate.js
@@ -1,35 +1,23 @@
-import { Component } from 'react'
 import Measure from 'react-measure'
 import Control from 'Containers/exchangeRate/control/Control'
 import Chart from 'Containers/exchangeRate/chart/Chart'
 
-
-export default class ExchangeRate extends Component {
-    componentDidMount() {
-        const {model, fetchCurrencyRateList} = this.props
-        fetchCurrencyRateList(model)
-    }
-
-    componentDidUpdate() {
-        const {model, fetchCurrencyRateList} = this.props
-        fetchCurrencyRateList(model)
-    }
-
-    render() {
-        const {model} = this.props
-        return (
+export const ExchangeRate = ({model, fetchCurrencyRateList}) => {
+    fetchCurrencyRateList(model)
+    return (
+        <div style={{display:'flex', flexDirection:'column', height: '100%'}}>
+            <Control model={model} />
             <Measure client>
                 {({measureRef, contentRect}) => (
-                    <div style={{display:'flex', flexDirection:'column', height: '100%'}}>
-                        <Control model={model} />
-                        <div ref={measureRef} style={{flex:1, height: 0}}>
-                            <Chart widgetId={model.id}
-                                width={contentRect.client.width || 10}
-                                height={contentRect.client.height - 10 || 20} />
-                        </div>
+                    <div ref={measureRef} style={{flex:1, height: 0}}>
+                        <Chart widgetId={model.id}
+                            width={contentRect.client.width || 10}
+                            height={contentRect.client.height - 10 || 20} />
                     </div>
                 )}
             </Measure>
-        )
-    }
+        </div>
+    )
 }
+
+export default ExchangeRate
